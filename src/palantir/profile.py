ebe1c1a81143af568e3dc59fc202b0399ad72d81
richardjlyon <richard@richardlyon.net>
2018-02-20 03:06:01 +0000

    tidy

--- src/palantir/profile.py
+++ src/palantir/profile.py
@@ -11,10 +11,16 @@ GAS_WELL_INITIAL_DI = 0.000880626223092  # TODO check this
 
 
 def _decline(di, t, qoi, b):
+    """Arp's equation for general decline in a well
+        - qoi: initial rate of production
+        - di: iniial decline rate
+        - b: curvature (b=0 exponential)
+    """
     return qoi / ((1 + b * di * t) ** (1 / b))
 
 
 def _zero_function(di, t, qoi, b, uor):
+    """Solve di to yield uor total barrels"""
     qo = _decline(di, t, qoi, b)
     return qo.sum() - uor
 
@@ -106,7 +112,6 @@ class Profiles:
         df = pd.DataFrame(well_dict)
         df.index = df['date']
         del df['date']
-        # return df
 
         self.curves = self.curves.append(df)
 
@@ -125,87 +130,3 @@ class Profiles:
     @property
     def well_production(self):
         return self.curves.groupby(['date', 'well'])['qo', 'qg'].sum().unstack(fill_value=0)
-
-
-class Profile:
-    """Represents a production profile for a Well"""
-
-    def __init__(self, well=None):
-        """Initialise a Profile"""
-        self.well = well
-        self._curves = None
-
-    @property
-    def curves(self):
-        return self._generate_curves()
-
-    def _generate_curves(self):
-
-        t = pd.Series(range(0, self.well.active_period))
-
-        if isinstance(self.well, OilWell):
-
-            # calculate di_oil
-            di_oil = optimize.newton(
-                    _zero_function,
-                    OIL_WELL_INITIAL_DI, args=(
-                        t,
-                        self.well.initial_oil_rate,
-                        self.well.b_oil,
-                        self.well.ultimate_oil_recovery))
-
-            # generate oil curve
-            qo = _decline(
-                    di_oil,
-                    t,
-                    self.well.initial_oil_rate,
-                    self.well.b_oil)
-
-            # generate gas curve
-            gor = self.well.gas_oil_ratio[0] + (
-                    self.well.gas_oil_ratio[1] - self.well.gas_oil_ratio[0]) * t / self.well.active_period
-            qg = qo * gor
-
-            # generate condensate curve
-            qc = pd.Series(np.zeros(self.well.active_period))
-
-        else:  # it's a gas well
-
-            # calculate di_gas
-            di_gas = optimize.newton(
-                    _zero_function,
-                    GAS_WELL_INITIAL_DI, args=(
-                        t,
-                        self.well.initial_gas_rate,
-                        self.well.b_gas,
-                        self.well.ultimate_gas_recovery))
-
-            # generate gas curve
-            qg = _decline(
-                    di_gas,
-                    t,
-                    self.well.initial_gas_rate,
-                    self.well.b_gas)
-
-            # generate condensate curve
-            qc = qg * self.well.gas_condensate_ratio
-
-            # generate oil curve
-            qo = pd.Series(np.zeros(self.well.active_period))
-
-        # pack into dataframe
-        well_dict = {
-            'asset': [self.well.asset.name.lower()] * self.well.active_period,
-            'pex': [self.well.pex.name.lower()] * self.well.active_period,
-            'whp': [self.well.whp.name.lower()] * self.well.active_period,
-            'well': [self.well.name.lower()] * self.well.active_period,
-            'date': pd.date_range(self.well.start_date, periods=self.well.active_period),
-            'qo': qo,
-            'qg': qg,
-            'qc': qc
-        }
-
-        df = pd.DataFrame(well_dict)
-        df.index = df['date']
-        del df['date']
-        return df
